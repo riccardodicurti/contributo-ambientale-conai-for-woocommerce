29edd6573c76670532c8ccf4f317e8baa95835f8
riccardodicurti <info@riccardodicurti.it>
2024-08-18 11:58:10 +0200

    1.1.3

--- contributo-ambientale-conai-for-woocommerce.php
+++ contributo-ambientale-conai-for-woocommerce.php
@@ -7,7 +7,7 @@
  * Description:       Calcolo del contributo ambientale conai per WooCommerce
  * Version:           1.1.3
  * Author:            Riccardo Di Curti
- * Author URI:        https://riccardodicurti.it/wc_conai
+ * Author URI:        https://riccardodicurti.it/
  * License: 		  GPLv2 or later
  * License URI: 	  https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       contributo-ambientale-conai-for-woocommerce
